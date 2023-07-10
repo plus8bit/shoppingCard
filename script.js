2610def8e5f300a097272750397cf27fb9f0376d
Nick <plus0bit@gmail.com>
2023-07-10 23:24:23 +0300

    Add styling

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
+addButtonEl.addEventListener('click', () => {
+  console.log(inputFieldEl.value)
+})
